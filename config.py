4d641a3340b83bef1b68e3a3f4db5640c4b75a6a
rrahulg <rahulgeorge1212@gmail.com>
2025-03-25 18:58:45 +0530

    [UPDATED] config file

--- config.py
+++ config.py
@@ -11,9 +11,39 @@ WEB_SEARCH_AGENT = {
     "add_refrence": True,
     "prevent_hallucinations": True,
 }
+
+EMAIL_AGENT = {
+    "name": "Email Agent",  # Name of the agent (e.g., "EmailAgent")
+    "role": "Send an email to the selected candidate",  # Role of the agent (e.g., "Send Email to the Selected Candidate")
+    "receiver_email": "",  # Recipient's email address
+    "sender_email": "",  # Sender's email address
+    "sender_name": "",  # Sender's name
+    "markdown": True,  # Enable Markdown formatting (default: True)
+    "verbose": True,  # Enable verbose mode (default: True)
+}
+
+GREETING_AGENT = {
+    "name": "Greeting agent",
+    "role": "You need to greet the user or bid them goodbye based on the input",
+}
+KNOWLEDGE_BASE_AGENT = {
+    "name": "Knowledge Base Agent",
+    "role": "Answer the query from the given knowledge base",
+    "instruction": ["Search the knowledge base for information"],
+    "knowledge_base": None,
+    "search_knowledge": True,
+    "markdown": True,
+    "prevent_hallucination": True,
+}
 WEBSITE_KB = {
     "urls": ["https://docs.phidata.com/introduction"],
     "max_links": 10,
     "table_name": "website_documents",
     "db_url": "postgresql+psycopg://ai:ai@localhost:5532/ai",
+}
+import os
+JSON_KB = {
+    'path': os.getcwd()+'\data\data.json',
+    'table_name': 'ai.json_documents',
+    'db_url': 'postgresql+psycopg://ai:ai@localhost:5532/ai',
 }
